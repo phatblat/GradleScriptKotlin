223db48eb75e008e1afff357fddc5b2830d6cb37
Ben Chatelain <ben@octop.ad>
2017-06-21 17:43:54 -0700

    Add all

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+/*allprojects {
+    declareHelloTask()
+}
+
+subprojects {
+    hello.doLast {
+        println("- I depend on water")
+    }
+}*/
+
+/*task removeBatchFile(type: Delete) {
+    delete 'gradlew.bat'
+}
+
+wrapper {
+    gradleVersion = '4.0'
+    distributionType = 'ALL'
+    finalizedBy removeBatchFile
+}*/
---
+++ buildSrc/src/main/kotlin/HelloTask.kt
@@ -0,0 +1,24 @@
+// https://github.com/gradle/kotlin-dsl/blob/master/samples/multi-project-with-buildSrc/buildSrc/src/main/kotlin/HelloTask.kt
+
+import org.gradle.api.*
+import org.gradle.api.tasks.*
+import org.gradle.kotlin.dsl.*
+
+open class HelloTask : DefaultTask() {
+
+    init {
+        group = "My"
+        description = "Prints a description of ${project.name}."
+    }
+
+    @TaskAction
+    fun run() {
+        println("I'm ${project.name}")
+    }
+}
+
+fun Project.declareHelloTask() =
+    task<HelloTask>("hello")
+
+val Project.hello: HelloTask
+    get() = tasks["hello"] as HelloTask
