69444ed3a3ddde64b80901685e1f35a4f44a4900
Ben Chatelain <ben@octop.ad>
2017-06-24 10:34:03 -0700

    Configure the kotlin-dsl plugin in buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,3 +1,23 @@
+/*
 plugins {
-    `kotlin-dsl`
+    //`kotlin-dsl`
+    id("org.gradle.kotlin.kotlin-dsl") version "0.10.8"
 }
+*/
+
+buildscript {
+  build.loadExtraPropertiesOf(project)
+
+  val kotlinRepo: String by extra { "https://plugins.gradle.org/m2/" }
+  repositories {
+      maven { url = uri(kotlinRepo) }
+  }
+
+  val kotlinVersion: String by extra { "0.10.8" }
+  dependencies {
+    classpath("gradle.plugin.org.gradle.kotlin:gradle-kotlin-dsl-plugins:$kotlinVersion")
+    //classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.1.2-5")
+  }
+}
+
+apply plugin: "org.gradle.kotlin.kotlin-dsl"
